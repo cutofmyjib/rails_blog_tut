a4fa7a54749fb6ce859b02073287e5bf7f9cdbb8
cutofmyjib <thedianalozano@gmail.com>
2015-07-05 21:42:56 -0700

    Add update and edit views and methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,13 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -15,6 +23,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
